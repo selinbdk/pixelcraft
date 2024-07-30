79fc09acb46242d4dcdc873e8b14f362c57a0eeb
Sedat Budak <sbudak@cyh.com.tr>
2024-07-30 15:48:22 +0300

    chore: deleted unnecessary files

--- lib/core/constants/message_constants.dart
+++
@@ -1,4 +0,0 @@
-class MessageConstants {
-  static const String onboardingPrimaryMessage = "Generate anything what's in your mind now";
-  static const String onboardingSecondaryMessage = "An AI that developed to help you generate what's in your mind into beautiful visual";
-}
