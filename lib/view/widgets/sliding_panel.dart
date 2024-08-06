cefbc9470fd2dfdcdce50f1e1570c992b0abfce9
Sedat Budak <sbudak@cyh.com.tr>
2024-08-06 15:27:12 +0300

    feat: added Dialog

--- lib/view/widgets/sliding_panel.dart
+++ lib/view/widgets/sliding_panel.dart
@@ -53,7 +53,17 @@ class _SlidingPanel extends StatelessWidget {
             SizedBox(
               height: 64,
               child: AppButton(
-                onPressed: () {},
+                onPressed: () async {
+                  log('message');
+                  await showDialog<void>(
+                    context: context,
+                    builder: (context) => _DialogField(
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
+                    ),
+                  );
+                },
                 backgroundColor: ColorName.primaryBlue,
                 foregroundColor: ColorName.primaryLabel,
                 messages: context.l10n.generateButtonTitle,
@@ -66,76 +76,3 @@ class _SlidingPanel extends StatelessWidget {
     );
   }
 }
-
-
-
-
-
-//  return ClipRRect(
-//       borderRadius: BorderRadius.circular(20),
-//       child: Container(
-//         width: MediaQuery.of(context).size.width,
-//         height: MediaQuery.of(context).size.height*3/5,
-//         color: ColorName.primaryBackground,
-//         child: Stack(
-//           children: [
-//             BackdropFilter(
-//               filter: ImageFilter.blur(
-//                 sigmaX: 4,
-//                 sigmaY: 4,
-//               ),
-//               child: Container(
-//                 width: double.infinity,
-//                 decoration: BoxDecoration(
-//                   gradient: LinearGradient(
-//                     colors: [
-//                       Colors.white.withOpacity(0.15),
-//                       Colors.white.withOpacity(0.05),
-//                     ],
-//                   ),
-//                 ),
-//               ),
-//             ),
-//             Column(
-//               children: [
-//                 Row(
-//                   mainAxisAlignment: MainAxisAlignment.end,
-//                   children: [
-//                     Center(
-//                       child: Text(
-//                         AppLocalizations.of(context).promptTitle,
-//                         style: context.appTextTheme.displayMedium?.copyWith(
-//                           color: ColorName.primaryLabel,
-//                           fontWeight: FontWeight.bold,
-//                           fontSize: 17.sp,
-//                         ),
-//                       ),
-//                     ),
-//                     115.horizontalSpace,
-//                     AppIconButton(
-//                       icon: Assets.icons.close.svg(),
-//                       onPressed: () => Navigator.pop(context),
-//                     ),
-//                   ],
-//                 ),
-//                 Padding(
-//                   padding: AppPadding.pagePadding,
-//                   child: const TextBoxWidget(),
-//                 ),
-//                 Padding(
-//                   padding: AppPadding.pagePadding,
-//                   child: AppButton(
-//                     onPressed: () {},
-//                     backgroundColor: ColorName.primaryBlue,
-//                     foregroundColor: ColorName.primaryLabel,
-//                     messages: context.l10n.generateButtonTitle,
-//                     icon: Assets.icons.wand.svg(),
-//                   ),
-//                 ),
-//               ],
-//             ),
-//           ],
-//         ),
-//       ),
-//     );
-//   }
