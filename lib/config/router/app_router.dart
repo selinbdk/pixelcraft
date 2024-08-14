15f426e7438a8ce8efc4854ec2458dbcab07ff27
Sedat Budak <budak.selin3@gmail.com>
2024-08-14 12:29:15 +0300

    chore: added new route

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -1,7 +1,8 @@
 import 'package:auto_route/auto_route.dart';
+import 'package:flutter/material.dart';
 import 'package:pixelcraft/view/discover_view.dart';
 import 'package:pixelcraft/view/onboarding_view.dart';
-
+import 'package:pixelcraft/view/result_view.dart';
 part 'app_router.gr.dart';
 
 final appRouter = AppRouter();
@@ -20,5 +21,12 @@ class AppRouter extends RootStackRouter {
         AutoRoute(
           page: DiscoverRoute.page,
         ),
+
+        AutoRoute(
+          page: ResultRoute.page,
+        ),
+
+
+        
       ];
 }
--- lib/config/router/app_router.gr.dart
+++ lib/config/router/app_router.gr.dart
@@ -46,3 +46,49 @@ class OnboardingRoute extends PageRouteInfo<void> {
     },
   );
 }
+
+/// generated route for
+/// [ResultView]
+class ResultRoute extends PageRouteInfo<ResultRouteArgs> {
+  ResultRoute({
+    required TextEditingController controller,
+    Key? key,
+    List<PageRouteInfo>? children,
+  }) : super(
+          ResultRoute.name,
+          args: ResultRouteArgs(
+            controller: controller,
+            key: key,
+          ),
+          initialChildren: children,
+        );
+
+  static const String name = 'ResultRoute';
+
+  static PageInfo page = PageInfo(
+    name,
+    builder: (data) {
+      final args = data.argsAs<ResultRouteArgs>();
+      return ResultView(
+        controller: args.controller,
+        key: args.key,
+      );
+    },
+  );
+}
+
+class ResultRouteArgs {
+  const ResultRouteArgs({
+    required this.controller,
+    this.key,
+  });
+
+  final TextEditingController controller;
+
+  final Key? key;
+
+  @override
+  String toString() {
+    return 'ResultRouteArgs{controller: $controller, key: $key}';
+  }
+}
