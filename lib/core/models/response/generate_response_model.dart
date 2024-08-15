6058a94fa98ac3d03792832072d9c7343060ed46
Sedat Budak <budak.selin3@gmail.com>
2024-08-15 11:10:07 +0300

    chore: updated dependencies and removed objectbox package
    feat: added hive package

--- lib/core/models/response/generate_response_model.dart
+++ lib/core/models/response/generate_response_model.dart
@@ -1,21 +1,16 @@
 import 'package:json_annotation/json_annotation.dart';
-import 'package:objectbox/objectbox.dart';
-import 'package:pixelcraft/core/models/response/image_response_model.dart';
 
 part 'generate_response_model.g.dart';
 
-@Entity()
 @JsonSerializable()
 class GenerateResponseModel {
   GenerateResponseModel({
     this.id = 0,
   });
 
-  @Id()
-  int? id;
-  final ToMany<ImageResponseModel> artifacts = ToMany<ImageResponseModel>();
-
   factory GenerateResponseModel.fromJson(Map<String, dynamic> json) => _$GenerateResponseModelFromJson(json);
 
   Map<String, dynamic> toJson() => _$GenerateResponseModelToJson(this);
+
+  int? id;
 }
--- lib/core/models/response/image_response_model.dart
+++ lib/core/models/response/image_response_model.dart
@@ -1,35 +1,21 @@
 import 'package:json_annotation/json_annotation.dart';
-import 'package:objectbox/objectbox.dart';
-
 
 part 'image_response_model.g.dart';
 
-@Entity()
 @JsonSerializable()
 class ImageResponseModel {
   ImageResponseModel({
     this.base64,
     this.finishReason,
     this.seed,
-    this.id=0,
+    this.id = 0,
   });
 
-  @Id()
+  factory ImageResponseModel.fromJson(Map<String, dynamic> json) => _$ImageResponseModelFromJson(json);
+  Map<String, dynamic> toJson() => _$ImageResponseModelToJson(this);
+
   int? id;
   String? base64;
   String? finishReason;
   int? seed;
-
-
-
-
-factory ImageResponseModel.fromJson(Map<String, dynamic> json)=> _$ImageResponseModelFromJson(json);
-
-Map<String, dynamic> toJson() => _$ImageResponseModelToJson(this);
-
-
-
-
-
-
 }
