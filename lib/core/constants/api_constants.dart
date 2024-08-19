c35b415ba1180a1990ff57334815019dd63c7791
Sedat Budak <budak.selin3@gmail.com>
2024-08-19 17:44:19 +0300

    fix: fixed engine_id

--- lib/core/constants/api_constants.dart
+++ lib/core/constants/api_constants.dart
@@ -1,4 +1,4 @@
 class ApiConstants {
   static const String generateImagePath =
-      '/v1/generation/{stable-diffusion-xl-1024-v1-0}/text-to-image';
+      '/v1/generation/stable-diffusion-xl-1024-v1-0/text-to-image';
 }
